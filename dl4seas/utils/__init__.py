93da7fe971200e46ab7d095a05f3f7d29503dc6f
Nicolas Fauchereau <Nicolas.Fauchereau@gmail.com>
2020-08-19 16:24:35 +1200

    added a small utility to shift a datetime index to end or beginning of month

--- dl4seas/utils/__init__.py
+++ dl4seas/utils/__init__.py
@@ -1,2 +1,3 @@
 from .list_files import list_files
 from .roll_longitudes import roll_longitudes
+from .shift_time import shift_time
---
+++ dl4seas/utils/shift_time.py
@@ -0,0 +1,43 @@
+def shift_time(time, shift='Month End'):
+    """
+    shift_time shift a time index to the beginning of the end of the month
+
+    [extended_summary]
+
+    Parameters
+    ----------
+    time : DatetimeIndex or compatible object
+        The index to shift 
+    shift : str, optional
+        can be 'Month End' or 'Month Begin', by default 'Month End'
+
+    Returns
+    -------
+    time
+        the shifted datetime index 
+
+    Example
+    -------
+
+    >> dates = pd.date_range(start='1990-01-01', periods=5, freq='MS')
+    
+    >> dates
+
+    DatetimeIndex(['1990-01-01', '1990-02-01', '1990-03-01', '1990-04-01',
+               '1990-05-01'],
+              dtype='datetime64[ns]', freq='MS')
+
+    >> shift_time(dates, 'Month End') 
+
+    DatetimeIndex(['1990-01-31', '1990-02-28', '1990-03-31', '1990-04-30',
+               '1990-05-31'],
+              dtype='datetime64[ns]', freq=None)
+
+
+
+    """
+    if shift == 'Month End':
+        time = time + pd.offsets.MonthEnd(0)
+    elif shift == 'Month Begin': 
+        time = time + pd.offsets.MonthBegin(-1)
+    return time 
